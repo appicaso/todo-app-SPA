928c06382b5044e9c6e7f1376594ea55c2920003
Serge Sroujian <ssroujian@outlook.com>
2021-06-02 01:15:31 -0400

    Added todo (add) funct, fixed issues with script running before external loaded causing Null exceptions. Added styling of todo items

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const loginBtn = document.querySelector(".loginbtn");
-const signupBtn = document.querySelector(".signupbtn");
-const form = document.getElementsByTagName("form");
-const listContainer = document.querySelector(".lists");
-const listItems = document.querySelector(".list-items");
-const lists = JSON.parse(localStorage.getItem("lists")) || [];
-const registeredUsers = JSON.parse(localStorage.getItem("users")) || [];
+const loginBtn = document.querySelector('.loginbtn');
+const signupBtn = document.querySelector('.signupbtn');
+const form = document.getElementsByTagName('form');
+const listContainer = document.querySelector('.lists');
+const listItems = document.querySelector('.list-items');
+const listItem = document.querySelector('.list-item');
+const lists = JSON.parse(localStorage.getItem('lists')) || [];
+const registeredUsers = JSON.parse(localStorage.getItem('users')) || [];
 
 async function fetchHtml(url) {
   return await (await fetch(url)).text();
 }
 
 async function loadNewContent(clickedlink) {
-  const content = document.querySelector(".main-content");
+  const content = document.querySelector('.main-content');
 
-  content.innerHTML = await fetchHtml(clickedlink + ".html");
+  content.innerHTML = await fetchHtml(clickedlink + '.html');
 
-  if (location.hash === "#signup") {
-    form[0].addEventListener("submit", (event) => {
+  if (location.hash === '#signup') {
+    form[0].addEventListener('submit', (event) => {
       event.preventDefault();
       if (formValidation()) {
         registerUser();
@@ -24,39 +25,42 @@ async function loadNewContent(clickedlink) {
     });
   }
 
-  if (location.hash === "#login") {
-    form[0].addEventListener("submit", (event) => {
+  if (location.hash === '#login') {
+    form[0].addEventListener('submit', (event) => {
       event.preventDefault();
       loginUser(username, password);
     });
   }
 
-  if (location.hash === "#dashboard") {
+  if (location.hash === '#dashboard') {
     loggedInState();
   }
 
-  if (location.hash === "#lists") {
-    form[0].addEventListener("submit", (event) => {
+  if (location.hash === '#lists') {
+    form[0].addEventListener('submit', (event) => {
       event.preventDefault();
       createNewList();
-      location.hash = "todo";
+      location.hash = 'todo';
     });
   }
 
-  if (location.hash === "#todo") {
-    form[0].addEventListener("submit", (event) => {
+  if (location.hash === '#todo') {
+    form[0].addEventListener('submit', (event) => {
       event.preventDefault();
       createNewTodo();
     });
   }
 }
 
-listContainer.addEventListener("click", (e) => {
-  const clickedList = e.target.id;
+listContainer.addEventListener('click', (e) => {
+  const clickedListId = e.target.id;
 
-  if (e.target.tagName.toLowerCase() === "li") {
-    selectList(clickedList);
-    renderListView(clickedList);
+  if (e.target.tagName.toLowerCase() === 'div' || e.target.tagName.toLowerCase() === 'li') {
+    location.hash = 'todo';
+    selectList(clickedListId);
+    setTimeout(() => {
+      appendTodosToView(clickedListId);
+    }, 100);
   }
 });
 
@@ -65,12 +69,8 @@ const formValidation = () => {
   const errorMessage = [];
 
   userForm.forEach((formfield) => {
-    if (
-      formfield.getAttribute("type") == "text" ||
-      formfield.getAttribute("type") == "password" ||
-      formfield.getAttribute("type") == "email"
-    ) {
-      if (formfield.innerText == "") {
+    if (formfield.getAttribute('type') == 'text' || formfield.getAttribute('type') == 'password' || formfield.getAttribute('type') == 'email') {
+      if (formfield.innerText == '') {
         errorMessage.push(`the ${formfield.name} field is empty \n`);
       }
     }
@@ -86,20 +86,20 @@ const formValidation = () => {
 };
 
 const displayFormError = (errorMessage) => {
-  const errorMessageContainer = document.querySelector(".error");
+  const errorMessageContainer = document.querySelector('.error');
   errorMessage.forEach((error) => {
     errorMessageContainer.innerText += error;
-    errorMessageContainer.classList.add("show");
+    errorMessageContainer.classList.add('show');
   });
 };
 
 const registerUser = () => {
-  const inputFields = document.getElementsByTagName("input");
+  const inputFields = document.getElementsByTagName('input');
   const newUser = createNewUser(inputFields);
 
   registeredUsers.push(newUser);
-  localStorage.setItem("users", JSON.stringify(registeredUsers));
-  console.log("user is registered!");
+  localStorage.setItem('users', JSON.stringify(registeredUsers));
+  console.log('user is registered!');
 };
 
 function createNewUser(inputFields) {
@@ -114,16 +114,11 @@ function createNewUser(inputFields) {
 }
 
 const loginUser = (username, password) => {
-  const findUser = registeredUsers.find(
-    (user) => username.value === user.email
-  );
+  const findUser = registeredUsers.find((user) => username.value === user.email);
 
-  if (
-    findUser.email === username.value &&
-    findUser.password === password.value
-  ) {
-    console.log("user is Logged in!");
-    location.hash = "#dashboard";
+  if (findUser.email === username.value && findUser.password === password.value) {
+    console.log('user is Logged in!');
+    location.hash = '#dashboard';
     loggedInState();
   } else {
     displayFormError();
@@ -132,18 +127,18 @@ const loginUser = (username, password) => {
 
 const loggedInState = () => {
   appendListsToView();
-  const welcomeMsg = document.querySelector(".welcome-message");
+  const welcomeMsg = document.querySelector('.welcome-message');
   welcomeMsg.innerText = `Welcome to your dashboard, ${
-    JSON.parse(localStorage.getItem("users")).firstName
+    JSON.parse(localStorage.getItem('users')).firstName
   } \n Add or Select new todo's to get started!`;
 };
 
 const createNewList = () => {
-  const newListName = document.getElementById("list").value;
+  const newListName = document.getElementById('list').value;
   const newListObj = createListObj(newListName);
 
   lists.push(newListObj);
-  localStorage.setItem("lists", JSON.stringify(lists));
+  localStorage.setItem('lists', JSON.stringify(lists));
   appendListsToView();
   selectList(newListObj.id);
 };
@@ -162,14 +157,15 @@ const appendListsToView = () => {
   clearElementsFromView(listItems);
   //Append all lists to view
   lists.forEach((list) => {
-    const generatedDivcontainer = document.createElement("div");
-    const generatedLiElement = document.createElement("li");
+    const generatedDivcontainer = document.createElement('div');
+    const generatedLiElement = document.createElement('li');
 
-    generatedDivcontainer.classList.add("list-item-container");
+    generatedDivcontainer.classList.add('list-item-container');
     listItems.appendChild(generatedDivcontainer);
 
     generatedLiElement.id = list.id;
-    generatedLiElement.classList.add("list-item");
+    generatedDivcontainer.id = list.id;
+    generatedLiElement.classList.add('list-item');
     generatedLiElement.innerText = list.name;
     generatedDivcontainer.appendChild(generatedLiElement);
   });
@@ -182,51 +178,50 @@ function clearElementsFromView(element) {
 }
 
 const createNewTodo = () => {
-  const relatedListId = JSON.parse(localStorage.getItem("selected.listID"));
-  const newTodo = document.getElementById("todo").value;
+  const relatedListId = JSON.parse(localStorage.getItem('selected.listID'));
+  const newTodo = document.getElementById('todo').value;
 
-  const findMatchingId = lists.find(
-    (list) => list.id === relatedListId.toString()
-  );
+  const findMatchingId = lists.find((list) => list.id === relatedListId.toString());
 
   findMatchingId.todos.push({ id: generateUniqueID(), todoItem: newTodo });
-  localStorage.setItem("lists", JSON.stringify(lists));
+  localStorage.setItem('lists', JSON.stringify(lists));
   appendTodosToView(relatedListId);
 };
 
 function selectList(selectedListId) {
   const selectedListElement = document.getElementById(selectedListId);
-  selectedListElement.classList.add("selected");
-  localStorage.setItem("selected.listID", selectedListId);
-}
-
-function renderListView(listID) {
-  location.hash = "todo";
-  appendTodosToView(listID);
+  selectedListElement.classList.add('selected');
+  localStorage.setItem('selected.listID', selectedListId);
 }
 
 const appendTodosToView = (listID) => {
-  const todoItems = document.querySelector(".todo-items");
+  const todoItems = document.querySelector('.todo-items');
+  console.log(todoItems);
+  // Reset all LI elements containing todos.
+  if (document.body.contains(todoItems)) {
+    clearElementsFromView(todoItems);
+  }
 
-  // Reset all LI elements containing Lists.
-  clearElementsFromView(todoItems);
+  const findMatchingListId = lists.find((list) => list.id === listID.toString());
 
-  const findMatchingTodoId = lists.find(
-    (list) => list.id === listID.toString()
-  );
+  //Append all Todos to view
+  findMatchingListId.todos.forEach((todo) => {
+    const generatedDivcontainer = document.createElement('div');
+    const generatedLiElement = document.createElement('li');
 
-  //Append all lists to view
-  findMatchingTodoId.todos.forEach((todo) => {
-    const generatedLiElement = document.createElement("li");
+    generatedDivcontainer.classList.add('todo-item-container');
+    generatedDivcontainer.id = todo.id;
+    todoItems.appendChild(generatedDivcontainer);
+
     generatedLiElement.id = todo.id;
-    generatedLiElement.classList.add("todo-item");
+    generatedLiElement.classList.add('todo-itemsa');
     generatedLiElement.innerText = todo.todoItem;
-    todoItems.appendChild(generatedLiElement);
+    generatedDivcontainer.appendChild(generatedLiElement);
   });
 };
 
 // Event listeners
-window.addEventListener("hashchange", () => {
+window.addEventListener('hashchange', () => {
   const clickedlink = location.hash.substr(1);
   loadNewContent(clickedlink);
 });
